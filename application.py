764a32384811e8db777f753045cbb602b2ddebc3
ronierison.maciel <ronierison.maciel@gmail.com>
2022-07-18 00:42:09 -0300

    ajuste

--- application.py
+++ application.py
@@ -1,8 +1,12 @@
 from flask import Flask
 
-application = Flask(__name__)
+app = Flask(__name__)
 
 
-@application.route("/")
+@app.route("/")
 def hello_world():
-    return "Hello World"
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
